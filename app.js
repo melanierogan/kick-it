753af1b1c12a565cdb8e7cc9095a20c9b715f275
Melanie <melanie.rogan@ft.com>
2020-09-13 16:36:08 +0100

    give me that sweet sweet refresh token

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ passport.use(
 			const { provider, id, username, photos, product } = profile;
 			profile.accessToken = accessToken;
 			profile.id = id;
+			profile.refreshToken = refreshToken;
 			return done(null, profile);
 		},
 	),
@@ -96,26 +97,8 @@ app.get('/test', async function(req, res) {
 		accessToken: req.user.accessToken,
 		user: req.user,
 	});
-
-	// Get tracks in a playlist
-	// const getPlaylists = await spotifyApi.getUserPlaylists(req.user.id);
-
-	const getSpecificPlaylist = await spotifyApi.getPlaylist(
-		req.session.playlistID || '1DVLNQ0AoUnf7CkzMXTmLZ',
-	);
-	console.log(req.session.playlistID, 'why wont you');
-	const getCurrentTrack = await spotifyApi.getMyCurrentPlaybackState({});
-	console.log(getSpecificPlaylist.body.tracks.items, 'what we got?');
-	res.render('index.html', {
-		user: req.user,
-		playlistID: getSpecificPlaylist.body.uri,
-		// turnary or otherwise if image show it if not default image
-		playlistsName: getSpecificPlaylist.body.name,
-		playlistsImage: getSpecificPlaylist.body.images[0]
-			? getSpecificPlaylist.body.images[0].url
-			: 'https://via.placeholder.com/550',
-		playlist: getSpecificPlaylist.body.tracks.items,
-		// currentTrack: getCurrentTrack.body.item.name,
+	res.status(200).json({
+		name: 'accessToken',
 	});
 });
 
